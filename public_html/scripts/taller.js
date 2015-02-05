c7d2d1dd25f07a0e53a6ff02d39950bb761a3611
jdcardenas10 <jd.cardenas10@uniandes.edu.co>
2015-02-05 11:17:05 -0500

    TallerJvaScriptBasico

---
+++ public_html/scripts/taller.js
@@ -0,0 +1,42 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+             function calcular() {
+                var x, y;
+                x = document.getElementById("valor1").value;
+                y = document.getElementById("valor2").value;
+ 
+                if (isNaN(x)) {
+                    document.getElementById("msj").innerHTML = "<div class=\"alert alert-danger\"> <a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times</a>El valor 1 debe ser un número</div>";
+                }
+                else if (isNaN(y)) {
+                    document.getElementById("msj").innerHTML = "<div class=\"alert alert-danger\"> <a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times</a>El valor 2 debe ser un número</div>";
+                }
+                else
+                {
+                    var rta = parseInt(x)+parseInt(y);
+                    document.getElementById("msj").innerHTML = "<div class=\"alert alert-success\"> <a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times</a>La respuesta es: "+ rta +"</div>";
+                }
+            }
+            function concatenar() {
+                var x, y;
+                x = document.getElementById("valor1").value;
+                y = document.getElementById("valor2").value;
+                
+                var rta=x+y;
+                 document.getElementById("msj").innerHTML = "<div class=\"alert alert-success\"> <a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times</a>La respuesta es: "+ rta +"</div>";
+            }
+            function ordenar() {
+                var x, y;
+                x = document.getElementById("valor1").value;
+                y = document.getElementById("valor2").value;
+                
+                var array =[x,y];
+                array.sort();
+                
+                var rta= "El orden es: "+ array[0]+", "+array[1];
+                document.getElementById("msj").innerHTML = "<div class=\"alert alert-success\"> <a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times</a>"+ rta +"</div>";
+            }
+
